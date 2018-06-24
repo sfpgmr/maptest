eadadb7188f1122ce82c21aa55ff450bbb15cb90
sfpgmr <sfpgmr@example.com>
2018-06-25 06:44:32 +0900

    update

--- modules/maptest.mjs
+++ modules/maptest.mjs
@@ -6,7 +6,7 @@
     let initLat = 34.678395;
     let initLon =  135.4601305;
     let watchId;
-    let zoom = 17;
+    let zoom = 18;
 
     let loc = await new Promise((resolve,reject)=>{
       if(navigator.geolocation){
@@ -19,16 +19,17 @@
       }
     });
 
-    let map = L.map('map').setView(loc,zoom);
+    let map = L.map('map',{minZoom:6,maxZoom:18}).setView(loc,zoom);
     //地理院地図レイヤー追加
     L.tileLayer(
       'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
       {
-        attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>"
+        attribution: "<a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>",
+        detectRetina:true
       }
     ).addTo(map);
 
-    const marker = L.marker(loc).addTo(map);
+    const marker = L.circleMarker(loc,{radius:5,color:'red',fill:true,fillColor:'red'}).addTo(map);
     const startMarker = L.marker(loc).addTo(map);
 
     function updateLoc(position){
